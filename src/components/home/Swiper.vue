65304ea88206cf2d4ba7a6333ab99f6f8c625ba6
fxbin <fxbin123@gmail.com>
2020-02-19 12:12:32 +0800

    home swiper

---
+++ src/components/home/Swiper.vue
@@ -0,0 +1,53 @@
+<!-- The ref attr used to find the swiper instance -->
+<template>
+    <div class="wrapper">
+        <swiper :options="swiperOption">
+            <swiper-slide v-for="banner in banners" :key="banner.id">
+                <img class="swiper-img" :src="banner.imgUrl" alt=""/>
+            </swiper-slide>
+            <!-- Optional controls -->
+            <div class="swiper-pagination"  slot="pagination"></div>
+        </swiper>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'HomeSwiper',
+    data () {
+        return {
+
+            swiperOption: {
+                pagination: {
+                    el: '.swiper-pagination'
+                },
+                loop: true
+            },
+            banners: [
+                {
+                    id: '1',
+                    imgUrl: 'https://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20201/be6871c9b0b0ffab3ef2a86fa9688677.jpg'
+                },
+                {
+                    id: '2',
+                    imgUrl: 'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/201912/0b26e9760c2673c63c05e1efce1275ea.jpg_750x200_495c0a72.jpg'
+                }
+            ]
+        }
+    }
+}
+</script>
+
+<style lang="stylus" scoped>
+    .wrapper >>> .swiper-pagination-bullet-active
+        background: #ffffff
+    .wrapper
+        overflow: hidden
+        width: 100%
+        height: 0
+        padding-bottom: 31.25%
+        background: #cacaca
+    .swiper-img
+        width: 100%
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,19 @@
 <template>
   <div class="home">
     <home-header/>
+    <home-swiper/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HomeHeader from '@/components/home/Header.vue'
+import HomeSwiper from "@/components/home/Swiper";
 
 export default {
   name: 'Home',
   components: {
+    HomeSwiper,
     HomeHeader
   }
 }
